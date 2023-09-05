69d8d563d523648f1c9f5a5daf7fcd67bd76379c
charlesc234 <cahilcharles123@gmail.com>
2023-09-04 21:38:57 -0400

    Added pfp with modal to slug page

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -3,6 +3,8 @@ import Navbar from "@/components/Navbar";
 import "bootstrap/dist/css/bootstrap.css";
 import { usePathname } from "next/navigation";
 import useSWR from "swr";
+import "../globals.css";
+import { useEffect } from "react";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -11,10 +13,14 @@ export default function modelPage() {
     "http://localhost:1337/api/models?populate=*",
     fetcher
   );
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap");
+  }, []);
 
   const pathname = usePathname();
   const names = [];
   var pageExists = false;
+  var modelIndex;
 
   if (!data)
     return <div style={{ backgroundColor: "#000000", height: 1000 }}></div>;
@@ -22,12 +28,11 @@ export default function modelPage() {
   for (let i = 0; i < data.data.length; i++) {
     names.push(data.data[i].attributes.display_name);
   }
-  console.log("names: " + names);
-  console.log(pathname);
 
   for (let i = 0; i < names.length; i++) {
     if (pathname.toUpperCase() == "/" + names[i].toUpperCase()) {
       pageExists = true;
+      modelIndex = i;
     }
   }
 
@@ -35,7 +40,85 @@ export default function modelPage() {
     return (
       <div style={{ backgroundColor: "#000000" }}>
         <Navbar />
-
+        <div className="container" style={{ baackgroundColor: "#000000" }}>
+          <div className="row ms-1">
+            <button
+              type="button"
+              class="btn btn-primary"
+              data-bs-toggle="modal"
+              data-bs-target={"#exampleModal" + pathname.slice(1)}
+              style={{
+                width: 500,
+                borderRadius: 10,
+                backgroundColor: "#000000",
+                borderWidth: 0,
+                padding: 0,
+              }}
+            >
+              <img
+                style={{
+                  width: 500,
+                  height: 500,
+                  objectFit: "cover",
+                  padding: 0,
+                  borderWidth: 0,
+                  borderRadius: 12.5,
+                }}
+                src={
+                  "http://localhost:1337" +
+                  data.data[modelIndex].attributes.profile_pic.data.attributes
+                    .url
+                }
+                class="img-thumbnail mt-4"
+                alt="..."
+              />
+            </button>
+            <div
+              class="modal modal-xl fade"
+              style={{ backgroundColor: "rgba(0,0,0,0)" }}
+              id={"exampleModal" + pathname.slice(1)}
+              tabindex="-1"
+              aria-labelledby={"exampleModalLabel" + pathname.slice(1)}
+              aria-hidden="true"
+            >
+              <div
+                class="modal-dialog modal-xl"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  backgroundColor: "rgba(0,0,0,0)",
+                }}
+              >
+                <div
+                  class="modal-content modal-xl"
+                  style={{
+                    borderRadius: 25,
+                    width: "100%",
+                    borderWidth: 0,
+                    height: "100%",
+                    backgroundColor: "rgba(0,0,0,0)",
+                  }}
+                >
+                  <img
+                    class="d-block img-fluid mx-auto"
+                    style={{
+                      height: 925,
+                      borderRadius: 25,
+                      borderWidth: 0,
+                      padding: 0,
+                    }}
+                    src={
+                      "http://localhost:1337" +
+                      data.data[modelIndex].attributes.profile_pic.data
+                        .attributes.url
+                    }
+                    alt="Card image cap"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div style={{ height: 1000 }} />
       </div>
     );

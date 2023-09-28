fd5e8a0d14c46254329a7b53e5c2517aef136425
“CharlesCahill123” <charles.cahill.intern@sofwerx.org>
2023-09-27 20:34:25 -0400

    categories

--- app/home.js
+++ app/home.js
@@ -53,8 +53,8 @@ export default function Page() {
   const newDataArr = [];
   const arrRecords = [];
   const categories = [];
+  categories.push("Top");
 
-  categories.push("All")
   for(let i = 0; i < cats.data.data.length ; i++){
     categories.push(cats.data.data[i].attributes.Category_Name);
   }

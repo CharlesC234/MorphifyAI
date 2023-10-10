ce5bfc312112e74eaffa41b03611bc715044516d
“CharlesCahill123” <charles.cahill.intern@sofwerx.org>
2023-10-10 00:18:22 -0400

    mobile changes

--- app/[slug]/perModel.js
+++ app/[slug]/perModel.js
@@ -38,7 +38,7 @@ export default function PerModel({ newDataArr, modelIndex, data }) {
               backgroundColor: "#000000",
               borderRadius: "100%",
               width: "25%",
-              minWidth: 300,
+              minWidth: 250,
               height: "auto",
               borderWidth: 0,
               padding: 0,

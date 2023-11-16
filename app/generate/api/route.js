006cee1d504f51f563213e0c75764ae1489b0d0e
GravyPouch <29843812+GravyPouch@users.noreply.github.com>
2023-11-15 22:44:19 -0500

    added generate page

---
+++ app/generate/api/route.js
@@ -0,0 +1,33 @@
+export async function POST() {
+  const res = await fetch(process.env.SD_API, {
+    method: "POST",
+    withCredentials: true,
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + process.env.SD_KEY,
+    },
+    body: JSON.stringify({
+      input: {
+        prompt:
+          "1girl, 18 years old, beautiful japanese woman, famous japanese idol, kawaii,",
+        negative_prompt: "FastNegativeV2, bad-hands-5,",
+        width: 720,
+        height: 1080,
+        seed: 2517236928,
+        steps: 50,
+      },
+    }),
+  });
+
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    console.log(res);
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}

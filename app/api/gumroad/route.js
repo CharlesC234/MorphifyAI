b2dab1ee66639bdb8c00fe9276a7fbdf3fd45551
“CharlesCahill123” <charles.cahill.intern@sofwerx.org>
2024-01-25 21:27:14 -0500

    fixing build issue

--- app/api/gumroad/route.js
+++ app/api/gumroad/route.js
@@ -1,6 +1,9 @@
 // pages/api/gumroad/route.ts
+import { NextResponse } from "next/server";
 
-export default async function handler(req, res) {
+// To handle a POST request to /api
+export async function POST(request) {
+  // Do whatever you want
   fetch(process.env.API + `/api/discord-users/17`, {
     method: "PUT",
     headers: {
@@ -12,23 +15,6 @@ export default async function handler(req, res) {
       },
     }),
   });
-  if (req.method === 'POST') {
-    try {
-      // Get the data from the request body
-      const data = req.body;
-
-      // Print the data to the console
-      console.log('Received POST request data: ', data);
-
-      // Respond with a 200 status code
-      res.status(200).json({ message: 'Request received successfully' });
-    } catch (error) {
-      console.error('Error processing POST request:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  } else {
-    // Respond with a 405 Method Not Allowed if the request is not a POST
-    res.status(405).json({ error: 'Method Not Allowed' });
-  }
+  return NextResponse.json({ message: "Hello World" }, { status: 200 });
 }
 

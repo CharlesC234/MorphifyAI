3c5a13881509dc6b9292efe669223e6cf86f777a
“CharlesCahill123” <charles.cahill.intern@sofwerx.org>
2024-02-03 19:09:58 -0500

    fixed bug

--- components/ImageLayout.js
+++ components/ImageLayout.js
@@ -163,7 +163,7 @@ export default function ImageLayout(data) {
             </div>
           );
             }else{
-              return <></>
+              return <div key={index}></div>
             }
   
         } else {

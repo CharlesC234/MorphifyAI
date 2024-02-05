0293fb63cdb7fd39527d7bf91b3f5003cb62502a
“CharlesCahill123” <charles.cahill.intern@sofwerx.org>
2024-02-05 00:39:23 -0500

    fixing image upload for multiple images

--- app/generate/generate.js
+++ app/generate/generate.js
@@ -81,7 +81,6 @@ const getRandomOptionForAttribute = (attribute, index) => {
 };
 
 
-
 export default function Generate({ fields }) {
 
   const router = useRouter();
@@ -100,6 +99,18 @@ export default function Generate({ fields }) {
 
   const [userData, setUserData] = useState({attributes: {Premium: false, Generations: 0}});
   const [generations, setGenerations] = useState(0);
+  const [uploadedImages, setUploadedImages] = useState([]);
+
+  const handleChange = event => {
+    if (event.target.files && event.target.files[0]) {
+      const temp = [];
+      for(let i = 0; i < event.target.files.length; i++){
+        temp.push(event.target.files[i]);
+      }
+      setUploadedImages([...uploadedImages, ...temp]);
+      console.log(uploadedImages);
+    }
+  };
 
   useEffect(() => {
     if (
@@ -417,8 +428,9 @@ export default function Generate({ fields }) {
       </div>
       </div>
       <div className={`${page == "Upload" ? "" : "hidden"}`}>
-      <div class="flex items-center justify-center w-full mt-3">
-    <label style={{backgroundColor: 'rgba(31, 41, 55, .75)'}} for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-400 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-900 hover:bg-gray-100 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+{uploadedImages.length == 0 ? 
+    <div class="flex items-center justify-center w-full mt-3">
+ <label style={{backgroundColor: 'rgba(31, 41, 55, .75)'}} for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
         <div class="flex flex-col items-center justify-center pt-5 pb-6">
             <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
@@ -426,9 +438,31 @@ export default function Generate({ fields }) {
             <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
             <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
         </div>
-        <input id="dropzone-file" type="file" class="hidden" />
+        <input onChange={handleChange} id="dropzone-file" type="file" multiple class="hidden" />
     </label>
-</div> 
+</div> :
+<div className="grid grid-cols-3 gap-3">
+{[...uploadedImages, "/"].map((item) => {
+  if(item == "/"){
+    return(
+    <div class="flex items-center justify-center w-full mt-3 aspect-square" style={{borderRadius: '50%'}}>
+    <label style={{backgroundColor: 'rgba(31, 41, 55, .75)', borderRadius: '50%'}} for="dropzone-file" class="flex flex-col items-center justify-center w-full border-2 border-gray-400 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-900 hover:bg-gray-100 dark:hover:border-gray-500 dark:hover:bg-gray-600 aspect-square">
+        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+            <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+            </svg>
+            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+            <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+        </div>
+        <input onChange={handleChange} id="dropzone-file" type="file" multiple class="hidden" />
+    </label>
+</div> );
+  }else{
+   return( <img style={{width: 300, height: 300, borderRadius: '50%'}} className="mt-3" src={URL.createObjectURL(item)}></img>)
+  }
+})}
+</div>
+}
 
 
       {fields.slice(-2).map((item, outerIndex) => {
